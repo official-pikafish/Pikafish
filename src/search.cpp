e5756bd8c21e8a7a45d9bd0c3b8efb77c6f1002a
PikaCat <760758491@qq.com>
2022-08-23 21:39:33 +0800

    remove current move num output

--- src/search.cpp
+++ src/search.cpp
@@ -838,10 +838,6 @@ moves_loop: // When in check, search starts here
 
       ss->moveCount = ++moveCount;
 
-      if (rootNode && thisThread == Threads.main() && Time.elapsed() > 3000)
-          sync_cout << "info depth " << depth
-                    << " currmove " << UCI::move(move)
-                    << " currmovenumber " << moveCount + thisThread->pvIdx << sync_endl;
       if (PvNode)
           (ss+1)->pv = nullptr;
 
