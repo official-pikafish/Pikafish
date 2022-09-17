62b8a28cee1ee68ed96cb9fe2c65be990ecd9592
mstembera <MissingEmail@email>
2022-09-17 05:45:54 -0700

    Simplify EVASIONS scoring
    remove some multipliers & adjust, doesn't change the move ordering
    
    STC https://tests.stockfishchess.org/tests/view/6325c1c9b9c0caa5f4a759ae
    LLR: 2.94 (-2.94,2.94) <-1.75,0.25>
    Total: 192760 W: 51528 L: 51482 D: 89750
    Ptnml(0-2): 642, 20490, 54148, 20380, 720
    
    Credit to locutus2
    
    No functional change
    
    (cherry picked from commit 29295ecfd357b5421eb4f273761dce8d9661f130)

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -147,11 +147,12 @@ void MovePicker::score() {
       else // Type == PSEUDO_LEGAL
       {
           if (pos.capture(m))
-              m.value =  PieceValue[MG][pos.piece_on(to_sq(m))] - Value(pt);
+              m.value =  PieceValue[MG][pos.piece_on(to_sq(m))]
+                       - Value(pt)
+                       + (1 << 28);
           else
-              m.value =  2 * (*mainHistory)[pos.side_to_move()][from_to(m)]
-                       + 2 * (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)]
-                       - (1 << 28);
+              m.value =  (*mainHistory)[pos.side_to_move()][from_to(m)]
+                       + (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)];
       }
   }
 }
