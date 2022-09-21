563919c39fb8750cb544ccf37265bf8e0fb90370
PikaCat <760758491@qq.com>
2022-09-21 18:55:33 +0800

    Only use rule to judge the opponent side at root ply
    this avoids some false mates

--- src/position.cpp
+++ src/position.cpp
@@ -927,9 +927,14 @@ bool Position::is_repeated(Value& result, int ply) const {
         // Return a score if a position repeats once earlier.
         if (stp->key == st->key)
         {
-            if (perpetualThem || perpetualUs)
+            if (perpetualUs || perpetualThem)
             {
-                result = !perpetualUs ? mate_in(ply) : !perpetualThem ? mated_in(ply) : VALUE_DRAW;
+                // Only judge the opponent side at root ply
+                if (perpetualUs && !perpetualThem) {
+                    result = mated_in(ply);
+                    return ply == 1;
+                }
+                result = perpetualUs ? VALUE_DRAW : mate_in(ply);
                 return true;
             }
 
@@ -955,7 +960,12 @@ bool Position::is_repeated(Value& result, int ply) const {
                 // Return a score if a position repeats once earlier.
                 if (stp->key == st->key)
                 {
-                    result = (chaseThem || chaseUs) ? (!chaseUs ? mate_in(ply) : !chaseThem ? mated_in(ply) : VALUE_DRAW) : VALUE_DRAW;
+                    // Only judge the opponent side at root ply
+                    if (chaseUs && !chaseThem) {
+                        result = mated_in(ply);
+                        return ply == 1;
+                    }
+                    result = chaseThem ? (chaseUs ? VALUE_DRAW : mate_in(ply)) : VALUE_DRAW;
                     return true;
                 }
 
