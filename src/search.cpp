97ddad679dbfaabf8b0b13f6aae675bee1384fab
Dubslow <bunslow@gmail.com>
2022-10-07 05:44:29 -0500

    Remove depth condition from razoring
    The eval condition depends on depth anyways, so this patch is nearly (not quite) non-functional
    
    passed STC:
    TC: 10+0.1
    Total/Win/Draw/Lose: 9528 / 1854 / 5846 / 1828
    PTNML: 41 / 962 / 2745 / 962 / 54
    WinRate: 50.14%
    ELO: 0.70[-2.89, 4.21]
    LOS: 65.18
    LLR: 2.55[-2.94, 2.94]
    
    passed LTC:
    TC: 60+0.6
    Total/Win/Draw/Lose: 7446 / 1186 / 5114 / 1146
    PTNML: 4 / 589 / 2498 / 627 / 5
    WinRate: 50.27%
    ELO: 1.43[-2.04, 4.83]
    LOS: 79.24
    LLR: 3.09[-2.94, 2.94]

--- src/search.cpp
+++ src/search.cpp
@@ -648,8 +648,7 @@ namespace {
     // Step 6. Razoring.
     // If eval is really low check with qsearch if it can exceed alpha, if it can't,
     // return a fail low.
-    if (   depth <= 7
-        && eval < alpha - 349 - 244 * depth * depth)
+    if (eval < alpha - 349 - 244 * depth * depth)
     {
         value = qsearch<NonPV>(pos, ss, alpha - 1, alpha);
         if (value < alpha)
