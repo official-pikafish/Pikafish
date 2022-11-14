b209b84ba5930d5e8539c9b09423d139da5dabbb
VoyagerOne <excelgeek@gmail.com>
2022-11-14 08:11:27 -0500

    Simplification away Cutoff Reset

--- src/search.cpp
+++ src/search.cpp
@@ -1193,8 +1193,6 @@ moves_loop: // When in check, search starts here
               }
           }
       }
-      else
-         ss->cutoffCnt = 0;
 
 
       // If the move is worse than some previously searched move, remember it to update its stats later
