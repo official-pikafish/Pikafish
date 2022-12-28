a4aad5e7074a2c0aa3afd41e2312020e9027f16a
PikaCat <760758491@qq.com>
2022-12-28 09:23:37 +0800

    SPSA tuning evaluate parameters on 1+0.05 iter 6966

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -111,19 +111,19 @@ Value Eval::evaluate(const Position& pos, int* complexity) {
   Value v = NNUE::evaluate(pos, &nnueComplexity);
   // Blend nnue complexity with material complexity
   Value optimism = pos.this_thread()->optimism[pos.side_to_move()];
-  nnueComplexity = (  416 * nnueComplexity
-                    + 424 * abs(m - v)
+  nnueComplexity = (  396 * nnueComplexity
+                    + 415 * abs(m - v)
                     + (optimism > 0 ? int(optimism) * int(m - v) : 0)
                     ) / 1024;
   if (complexity) // Return hybrid NNUE complexity to caller
       *complexity = nnueComplexity;
 
-  int scale = 1068 + 147 * pos.material_sum() / 4096;
-  optimism = optimism * (275 + nnueComplexity) / 256;
-  v = (v * scale + optimism * (scale - 751)) / 1024;
+  int scale = 967 + 141 * pos.material_sum() / 4096;
+  optimism = optimism * (260 + nnueComplexity) / 256;
+  v = (v * scale + optimism * (scale - 854)) / 1024;
 
   // Damp down the evaluation linearly when shuffling
-  v = v * (119 - pos.rule60_count()) / 119;
+  v = v * (126 - pos.rule60_count()) / 120;
 
   // Guarantee evaluation does not hit the mate range
   v = std::clamp(v, VALUE_MATED_IN_MAX_PLY + 1, VALUE_MATE_IN_MAX_PLY - 1);
