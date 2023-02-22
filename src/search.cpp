2234fe743b838d65a1029b7bb313271120d129c3
Dubslow <bunslow@gmail.com>
2023-02-22 05:45:43 -0600

    Late counter bonus: boost underestimated moves
    The idea here is very intuitive: since we've just proven that the move is good, then if it previously had poor stats, boost those stats more than otherwise.

--- src/search.cpp
+++ src/search.cpp
@@ -1285,7 +1285,7 @@ moves_loop: // When in check, search starts here
     else if (!priorCapture)
     {
         // Extra bonuses for PV/Cut nodes or bad fail lows
-        int bonus = (depth > 4) + (PvNode || cutNode) + (bestValue < alpha - 80 * depth);
+        int bonus = (depth > 4) + (PvNode || cutNode) + (bestValue < alpha - 80 * depth) + ((ss-1)->moveCount > 10);
         update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, stat_bonus(depth) * bonus);
     }
 
