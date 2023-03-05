6e7f00bf00a435feff5e71c4cd45f148edeefd7c
PikaCat <760758491@qq.com>
2023-03-05 12:59:58 +0800

    Remove psq completely

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,6 @@
 
 #include "bitboard.h"
 #include "position.h"
-#include "material.h"
 #include "search.h"
 #include "thread.h"
 #include "uci.h"
@@ -31,7 +30,6 @@ int main(int argc, char* argv[]) {
   CommandLine::init(argc, argv);
   UCI::init(Options);
   Tune::init();
-  Material::init();
   Bitboards::init();
   Position::init();
   Threads.set(size_t(Options["Threads"]));
