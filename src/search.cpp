1fd72f72355894f157921625660eabe6211e6866
PikaCat <Pika-Cat@qq.com>
2023-05-08 10:33:40 +0800

    Refine deeper post-lmr searches
    This patch improves logic conditions for performing deeper searches after passed LMR.
    
    Instead of exceeding alpha by some margin now it requires to exceed the
    current best value - which may be lower than alpha (but never bigger since we
    update alpha with bestvalue if it exceeds alpha).

--- src/search.cpp
+++ src/search.cpp
@@ -1073,7 +1073,7 @@ moves_loop: // When in check, search starts here
           {
               // Adjust full depth search based on LMR results - if result
               // was good enough search deeper, if it was bad enough search shallower
-              const bool doDeeperSearch = value > (alpha + 47 + 9 * (newDepth - d));
+              const bool doDeeperSearch = value > (bestValue + 57 + 9 * (newDepth - d));
               const bool doEvenDeeperSearch = value > alpha + 648 && ss->doubleExtensions <= 5;
               const bool doShallowerSearch = value < bestValue + newDepth;
 
