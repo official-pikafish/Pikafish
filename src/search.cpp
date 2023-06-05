ee0a9fbd97bb2baa7a79333a47fb2840bbfd3cdb
PikaCat <Pika-Cat@qq.com>
2023-06-05 15:01:52 +0800

    Remove static eval threshold for extensions when giving check

--- src/search.cpp
+++ src/search.cpp
@@ -959,8 +959,7 @@ moves_loop: // When in check, search starts here
 
           // Check extensions (~1 Elo)
           else if (   givesCheck
-                   && depth > 8
-                   && abs(ss->staticEval) > 66)
+                   && depth > 9)
               extension = 1;
 
           // Quiet ttMove extensions (~0 Elo)
