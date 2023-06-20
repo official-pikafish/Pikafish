6fd223378e17c380abe9961d0a6cd84076164ffd
PikaCat <Pika-Cat@qq.com>
2023-06-20 17:00:27 +0800

    Eval parameter tuning
    Book: 3mvs 251-350_51933
    TC: 30+0.2
    Total/Win/Draw/Lose: 5164 / 1271 / 2730 / 1163
    PTNML: 7 / 580 / 1306 / 676 / 13
    WinRate: 51.05%
    ELO: 6.67[1.42, 11.85]
    LOS: 99.35
    LLR: 3.26[-2.94, 2.94]
    
    Book: 3mvs 251-350_51933
    TC: 60+0.6
    Total/Win/Draw/Lose: 6414 / 1592 / 3331 / 1491
    PTNML: 8 / 671 / 1746 / 776 / 6
    WinRate: 50.79%
    ELO: 4.87[0.47, 9.16]
    LOS: 98.47
    LLR: 3.02[-2.94, 2.94]

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -115,10 +115,10 @@ Value Eval::evaluate(const Position& pos) {
   // Scale nnue score according to material and optimism
   Value optimism = pos.this_thread()->optimism[pos.side_to_move()] * abs(pos.material_diff() - v) / 512;
   Value material = pos.material_sum() / 64;
-  v = (v * (591 + material) + optimism * (165 + material)) / 1024;
+  v = (v * (601 + material) + optimism * (184 + material)) / 1024;
 
   // Damp down the evaluation linearly when shuffling
-  v = v * (203 - pos.rule60_count()) / 135;
+  v = v * (240 - pos.rule60_count()) / 136;
 
   // Guarantee evaluation does not hit the mate range
   v = std::clamp(v, VALUE_MATED_IN_MAX_PLY + 1, VALUE_MATE_IN_MAX_PLY - 1);
