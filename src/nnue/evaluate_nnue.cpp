03aa36c2231290628a4d9df892a3d279090b2e43
PikaCat <Pika-Cat@qq.com>
2023-08-23 09:25:18 +0800

    Eval parameters tuning
    Book: 3mvs 251-350_51933
    TC: 100+1
    Total/Win/Draw/Lose: 6242 / 1630 / 3100 / 1512
    PTNML: 8 / 626 / 1733 / 748 / 6
    WinRate: 50.95%
    ELO: 6.07[1.80, 10.29]
    LOS: 99.72
    LLR: 2.93[-2.94, 2.94]

--- src/nnue/evaluate_nnue.cpp
+++ src/nnue/evaluate_nnue.cpp
@@ -148,7 +148,7 @@ namespace Stockfish::Eval::NNUE {
     // overaligning stack variables with alignas() doesn't work correctly.
 
     constexpr uint64_t alignment = CacheLineSize;
-    constexpr int delta = 24;
+    constexpr int delta = 26;
 
 #if defined(ALIGNAS_ON_STACK_VARIABLES_BROKEN)
     TransformedFeatureType transformedFeaturesUnaligned[
