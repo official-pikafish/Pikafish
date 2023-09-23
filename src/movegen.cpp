f78fd809b6e9e7261a8e941daa3f4fdb56e93107
PikaCat <Pika-Cat@qq.com>
2023-09-23 12:11:57 +0800

    Make casting styles consistent
    Make casting styles consistent with the rest of the code.
    
    No functional change

--- src/movegen.cpp
+++ src/movegen.cpp
@@ -124,6 +124,7 @@ template<GenType Type>
 ExtMove* generate(const Position& pos, ExtMove* moveList) {
 
   static_assert(Type != LEGAL, "Unsupported type in generate()");
+  assert((Type == EVASIONS) == bool(pos.checkers()));
 
   Color us = pos.side_to_move();
 
