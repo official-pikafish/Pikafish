7e69896d73edca6c4a0180a8d0f178f1f4822055
PikaCat <73384062+PikaCat-OuO@users.noreply.github.com>
2023-11-14 10:05:18 +0800

    Do more check in rule mate
    Solve situations like:
    A - root - A - A
    Before resolution, when root reaches the second A, it will affect the result.
    
    R1nk1ab2/4a4/4b4/1C7/p8/2P1P3P/9/4B4/9/3rKAB2 w - - 3 3

--- src/position.cpp
+++ src/position.cpp
@@ -1067,7 +1067,7 @@ bool Position::rule_judge(Value& result, int ply) const {
                     result = !checkUs ? mate_in(ply) : !checkThem ? mated_in(ply) : VALUE_DRAW;
 
                 // Catch false mates
-                if (result > VALUE_MATED_IN_MAX_PLY || cnt == 2)
+                if (result == VALUE_DRAW || (result > VALUE_DRAW && filter[st->key] <= 1) || cnt == 2)
                     return true;
                 // We know there can't be another fold
                 if (filter[st->key] <= 1)
