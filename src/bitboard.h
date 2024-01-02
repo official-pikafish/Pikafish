e0597293109dee7a7fe3e44e882ffc5dc4bd9658
PikaCat <Pika-Cat@qq.com>
2024-01-02 13:06:03 +0800

    Mark square_bb() as constexpr
    No functional change

--- src/bitboard.h
+++ src/bitboard.h
@@ -102,7 +102,7 @@ extern Magic BishopMagics[SQUARE_NB];
 extern Magic KnightMagics[SQUARE_NB];
 extern Magic KnightToMagics[SQUARE_NB];
 
-inline Bitboard square_bb(Square s) {
+constexpr Bitboard square_bb(Square s) {
     assert(is_ok(s));
     return SquareBB[s];
 }
