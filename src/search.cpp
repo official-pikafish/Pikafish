9afea0e8900ef3df3fd6ed4b8c29822d94ef8871
PikaCat <Pika-Cat@qq.com>
2024-01-23 23:07:45 +0800

    Reduce futility_margin further when improving
    The idea of this is to unroll the futility_margin calculation to allow
    for the improving flag to have a greater effect on the futility margin.
    The current factor is 1.5 instead of the previous 1 resulting in a
    deduction of an extra margin/2 from futilit_margin if improving. The
    chosen value was not tuned, meaning that there is room for tweaking it.
    This patch is partially inspired by Vizvezdenec, who, although quite
    different in execution, tested another idea where the futility_margin is
    lowered further when improving.

--- src/search.cpp
+++ src/search.cpp
@@ -50,7 +50,8 @@ namespace {
 
 // Futility margin
 Value futility_margin(Depth d, bool noTtCutNode, bool improving) {
-    return ((140 - 30 * noTtCutNode) * (d - improving));
+    Value futilityMult = 140 - 30 * noTtCutNode;
+    return (futilityMult * d - 3 * futilityMult / 2 * improving);
 }
 
 constexpr int futility_move_count(bool improving, Depth depth) {
