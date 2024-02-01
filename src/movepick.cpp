850b28bffcc1cf6e6d0c4182ad2be4c6316e5208
Stefan Geschwentner <stgeschwentner@gmail.com>
2024-02-01 21:09:48 +0100

    Tweak capture scoring for move ordering
    Move divisor from capture scoring to good capture
    check and sligthly increase it.
    
    This has several effects:
    - its a speedup because for quience and probcut
      search the division now never happens. For main
      search its delayed and can be avoided if a good
      capture triggers a cutoff
    - through the higher resolution of scores we have
      a more granular sorting

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -169,9 +169,8 @@ void MovePicker::score() {
     {
         if constexpr (Type == CAPTURES)
             m.value =
-              (7 * int(PieceValue[pos.piece_on(m.to_sq())])
-               + (*captureHistory)[pos.moved_piece(m)][m.to_sq()][type_of(pos.piece_on(m.to_sq()))])
-              / 16;
+              7 * int(PieceValue[pos.piece_on(m.to_sq())])
+              + (*captureHistory)[pos.moved_piece(m)][m.to_sq()][type_of(pos.piece_on(m.to_sq()))];
 
         else if constexpr (Type == QUIETS)
         {
@@ -273,7 +272,8 @@ top:
     case GOOD_CAPTURE :
         if (select<Next>([&]() {
                 // Move losing capture to endBadCaptures to be tried later
-                return pos.see_ge(*cur, -cur->value) ? true : (*endBadCaptures++ = *cur, false);
+                return pos.see_ge(*cur, -cur->value / 18) ? true
+                                                          : (*endBadCaptures++ = *cur, false);
             }))
             return *(cur - 1);
 
