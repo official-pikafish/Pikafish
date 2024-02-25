7bf46366248d0d00755e4de2fc3727f6c388109d
dhb <1084714805@qq.com>
2024-02-26 01:38:05 +0800

    try relaxed int8 dot product intrinsic

--- src/emscripten/wasm_simd.cpp
+++ src/emscripten/wasm_simd.cpp
@@ -18,6 +18,14 @@ auto dot_i8x16(__i8x16 a, __i8x16 b) -> __i32x4 {
   return wasm_i32x4_add(wasm_i32x4_dot_i16x8(a_lo, b_lo), wasm_i32x4_dot_i16x8(a_hi, b_hi));
 };
 
+auto dot_i8x16_add(__i32x4 sum, __i8x16 a, __i8x16 b) -> __i32x4 {
+#ifdef __wasm_relaxed_simd__
+  return __builtin_wasm_relaxed_dot_i8x16_i7x16_add_s_i32x4(a, b, sum);
+#else
+  return wasm_i32x4_add(sum, dot_i8x16(a, b));
+#endif
+};
+
 auto dot_i16x8(__i16x8 a, __i16x8 b) -> __i32x4 {
   __i16x8 c = wasm_i16x8_mul(a, b);
   return wasm_i32x4_add(wasm_i32x4_extend_low_i16x8(c), wasm_i32x4_extend_high_i16x8(c));
@@ -41,7 +49,7 @@ template<int n>
 void dot(const int8_t* a, const uint8_t* x, const int32_t* b, int32_t* out) {
   __i32x4 z = wasm_i32x4_splat(0);
   for (int j = 0; j < n; j += 16) {
-    z = wasm_i32x4_add(z, dot_i8x16(wasm_v128_load(&a[j]), wasm_v128_load(&x[j])));
+    z = dot_i8x16_add(z, wasm_v128_load(&a[j]), wasm_v128_load(&x[j]));
   }
   out[0] = b[0] + z[0] + z[1] + z[2] + z[3];
 };
@@ -57,10 +65,10 @@ void dot4(const int8_t* a, const uint8_t* x, const int32_t* b, int32_t* out) {
   __i32x4 z3 = wasm_i32x4_splat(0);
   for (int j = 0; j < n; j += 16) {
     __i8x16 xv = wasm_v128_load(&x[j]);
-    z0 = wasm_i32x4_add(z0, dot_i8x16(wasm_v128_load(&a[j + 0 * n_stride]), xv));
-    z1 = wasm_i32x4_add(z1, dot_i8x16(wasm_v128_load(&a[j + 1 * n_stride]), xv));
-    z2 = wasm_i32x4_add(z2, dot_i8x16(wasm_v128_load(&a[j + 2 * n_stride]), xv));
-    z3 = wasm_i32x4_add(z3, dot_i8x16(wasm_v128_load(&a[j + 3 * n_stride]), xv));
+    z0 = dot_i8x16_add(z0, wasm_v128_load(&a[j + 0 * n_stride]), xv);
+    z1 = dot_i8x16_add(z1, wasm_v128_load(&a[j + 1 * n_stride]), xv);
+    z2 = dot_i8x16_add(z2, wasm_v128_load(&a[j + 2 * n_stride]), xv);
+    z3 = dot_i8x16_add(z3, wasm_v128_load(&a[j + 3 * n_stride]), xv);
   }
   __i32x4 z = wasm_i32x4_add(wasm_v128_load(&b[0]), haddx4(z0, z1, z2, z3));
   wasm_v128_store(&out[0], z);
