7846d91fefa00b8d5b99204f2ca3bfeab59feb04
Disservin <disservin.social@gmail.com>
2024-03-23 10:22:20 +0100

    Transform search output to engine callbacks
    Part 2 of the Split UCI into UCIEngine and Engine refactor.
    This creates function callbacks for search to use when an update should occur.
    The benching in uci.cpp for example does this to extract the total nodes
    searched.
    
    No functional change

--- src/score.cpp
+++ src/score.cpp
@@ -1,41 +1,43 @@
 /*
   Stockfish, a UCI chess playing engine derived from Glaurung 2.1
   Copyright (C) 2004-2024 The Stockfish developers (see AUTHORS file)
+
   Stockfish is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
+
   Stockfish is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
+
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <iostream>
+#include "score.h"
 
-#include "bitboard.h"
-#include "misc.h"
-#include "position.h"
-#include "types.h"
+#include <cassert>
+#include <cmath>
+#include <cstdlib>
+
 #include "uci.h"
-#include "tune.h"
 
-using namespace Stockfish;
+namespace Stockfish {
 
-int main(int argc, char* argv[]) {
+Score::Score(Value v, const Position& pos) {
+    assert(-VALUE_INFINITE < v && v < VALUE_INFINITE);
 
-    std::cout << engine_info() << std::endl;
+    if (std::abs(v) < VALUE_MATE_IN_MAX_PLY)
+    {
+        score = InternalUnits{UCIEngine::to_cp(v, pos)};
+    }
+    else
+    {
+        auto distance = VALUE_MATE - std::abs(v);
+        score         = (v > 0) ? Mate{distance} : Mate{-distance};
+    }
+}
 
-    Bitboards::init();
-    Position::init();
-
-    UCIEngine uci(argc, argv);
-
-    Tune::init(uci.engine_options());
-
-    uci.loop();
-
-    return 0;
 }
--- src/score.h
+++ src/score.h
@@ -1,41 +1,65 @@
 /*
   Stockfish, a UCI chess playing engine derived from Glaurung 2.1
   Copyright (C) 2004-2024 The Stockfish developers (see AUTHORS file)
+
   Stockfish is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
+
   Stockfish is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
+
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <iostream>
+#ifndef SCORE_H_INCLUDED
+#define SCORE_H_INCLUDED
 
-#include "bitboard.h"
-#include "misc.h"
-#include "position.h"
+#include <variant>
+#include <utility>
+
 #include "types.h"
-#include "uci.h"
-#include "tune.h"
 
-using namespace Stockfish;
+namespace Stockfish {
 
-int main(int argc, char* argv[]) {
+class Position;
 
-    std::cout << engine_info() << std::endl;
+class Score {
+   public:
+    struct Mate {
+        int plies;
+    };
 
-    Bitboards::init();
-    Position::init();
+    struct InternalUnits {
+        int value;
+    };
 
-    UCIEngine uci(argc, argv);
+    Score() = default;
+    Score(Value v, const Position& pos);
 
-    Tune::init(uci.engine_options());
+    template<typename T>
+    bool is() const {
+        return std::holds_alternative<T>(score);
+    }
 
-    uci.loop();
+    template<typename T>
+    T get() const {
+        return std::get<T>(score);
+    }
 
-    return 0;
+    template<typename F>
+    decltype(auto) visit(F&& f) const {
+        return std::visit(std::forward<F>(f), score);
+    }
+
+   private:
+    std::variant<Mate, InternalUnits> score;
+};
+
 }
+
+#endif  // #ifndef SCORE_H_INCLUDED
