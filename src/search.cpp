f28a9e2f7d27a390a0ead8f5a42e2d2ccbd9c517
Shawn Xu <xu107288696@gmail.com>
2024-05-15 22:26:15 -0700

    Tweak NMP Formula

--- src/search.cpp
+++ src/search.cpp
@@ -683,7 +683,7 @@ Value Search::Worker::search(
         assert(eval - beta >= 0);
 
         // Null move dynamic reduction based on depth and eval
-        Depth R = std::min(int(eval - beta) / 122, 5) + depth / 3 + 4;
+        Depth R = std::min(int(eval - beta) / 122, 5) + depth / 3 + 5;
 
         ss->currentMove         = Move::null();
         ss->continuationHistory = &thisThread->continuationHistory[0][0][NO_PIECE][0];
