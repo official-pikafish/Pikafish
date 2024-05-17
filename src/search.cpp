91becb9ed31636f641728106ad6264762e8ed3e1
PikaCat <Pika-Cat@qq.com>
2024-05-17 13:23:47 +0800

    Simplifying improving and worsening deduction formulas

--- src/search.cpp
+++ src/search.cpp
@@ -57,8 +57,8 @@ static constexpr double EvalLevel[10] = {0.981, 0.956, 0.895, 0.949, 0.913,
 // Futility margin
 Value futility_margin(Depth d, bool noTtCutNode, bool improving, bool oppWorsening) {
     Value futilityMult       = 158 - 33 * noTtCutNode;
-    Value improvingDeduction = 44 * improving * futilityMult / 32;
-    Value worseningDeduction = (364 + 47 * improving) * oppWorsening * futilityMult / 1024;
+    Value improvingDeduction = 2 * improving * futilityMult;
+    Value worseningDeduction = 392 * oppWorsening * futilityMult / 1024;
 
     return futilityMult * d - improvingDeduction - worseningDeduction;
 }
