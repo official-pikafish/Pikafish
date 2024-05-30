b9809509735d4e1fe2ec748f5db4a3ed31013e26
Viren6 <94880762+Viren6@users.noreply.github.com>
2024-05-30 08:18:04 +0100

    Add extension condition to cutoffCnt
    Decrease cutoffCnt increment by 1 if extension is 2 or greater.

--- src/search.cpp
+++ src/search.cpp
@@ -1184,7 +1184,7 @@ moves_loop:  // When in check, search starts here
 
                 if (value >= beta)
                 {
-                    ss->cutoffCnt += 1 + !ttMove;
+                    ss->cutoffCnt += 1 + !ttMove - (extension >= 2);
                     assert(value >= beta);  // Fail high
                     break;
                 }
