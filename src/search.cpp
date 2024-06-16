120f872ac4b45c9b733e703d350dd6a58762ffb6
Shawn Xu <xu107288696@gmail.com>
2024-06-16 16:14:22 -0700

    Simplify Bonus Formula In History Adjustment
    Inspired by a discord message [1] from Vizvezdenec, this patch simplifies the
    bonus adjustment bonus = bonus > 0 ? 2 * bonus : bonus / 2 to a constant
    addition, maintaining bonus average at around 0 in regular bench. As cj5716
    pointed in discord [2], the constant bonus can also be considered as factoring
    tempo when calculating bonus, yielding a better value of the move.

--- src/search.cpp
+++ src/search.cpp
@@ -643,8 +643,7 @@ Value Search::Worker::search(
     // Use static evaluation difference to improve quiet move ordering (~9 Elo)
     if (((ss - 1)->currentMove).is_ok() && !(ss - 1)->inCheck && !priorCapture)
     {
-        int bonus = std::clamp(-12 * int((ss - 1)->staticEval + ss->staticEval), -1003, 1820);
-        bonus     = bonus > 0 ? 2 * bonus : bonus / 2;
+        int bonus = std::clamp(-12 * int((ss - 1)->staticEval + ss->staticEval), -1003, 1820) + 800;
         thisThread->mainHistory[~us][((ss - 1)->currentMove).from_to()] << bonus;
         if (type_of(pos.piece_on(prevSq)) != PAWN)
             thisThread->pawnHistory[pawn_structure_index(pos)][pos.piece_on(prevSq)][prevSq]
