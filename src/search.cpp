e3022cb4a0692b7cf46f5c76caf40b18e7cf9775
PikaCat <Pika-Cat@qq.com>
2024-07-14 19:52:34 +0800

    Remove quiet history pruning depth limit

--- src/search.cpp
+++ src/search.cpp
@@ -905,7 +905,7 @@ moves_loop:  // When in check, search starts here
                   + thisThread->pawnHistory[pawn_structure_index(pos)][movedPiece][move.to_sq()];
 
                 // Continuation history based pruning (~2 Elo)
-                if (lmrDepth < 7 && history < -3653 * depth)
+                if (history < -3653 * depth)
                     continue;
 
                 history += 2 * thisThread->mainHistory[us][move.from_to()];
