7a2ab13dd0327f868f7b97982c96fb5100c72a62
Shawn Xu <xu107288696@gmail.com>
2024-07-20 12:41:56 -0700

    Remove Killers
    The removal of killers resulted in a substantial decrease
    in pre-LMR history average, so a negative history fill is applied to
    counter it.

--- src/search.h
+++ src/search.h
@@ -64,7 +64,6 @@ struct Stack {
     int             ply;
     Move            currentMove;
     Move            excludedMove;
-    Move            killer;
     Value           staticEval;
     int             statScore;
     int             moveCount;
