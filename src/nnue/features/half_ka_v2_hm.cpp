52aed4c9f2de4f355098681cce04dcfdb19b851a
PikaCat <Pika-Cat@qq.com>
2024-08-28 13:32:55 +0800

    Upgrade ATT feature to ATT++
    Improve ATT with:
    1. Introduce mirroring for the opponent king when our king is in the middle file.
    2. Enhanced input feature representation based on attack information.
    3. LayersStack selection based on attack information.
    
    This architecture is jointly devised by 妙神 @iaoedsz2008, 以往 @subarashikihibi, PikaCat @PikaCat-OuO, 陶然 @pickaxe919，credit to them, thanks for their great work!

--- src/nnue/features/half_ka_v2_hm.cpp
+++ src/nnue/features/half_ka_v2_hm.cpp
@@ -26,37 +26,49 @@
 
 namespace Stockfish::Eval::NNUE::Features {
 
+// Get attack bucket
+IndexType HalfKAv2_hm::make_attack_bucket(const Position& pos, Color c) {
+    return AttackBucket[pos.count<ROOK>(c)][pos.count<KNIGHT>(c)][pos.count<CANNON>(c)];
+}
+
+// Get layer stack bucket
+IndexType HalfKAv2_hm::make_layer_stack_bucket(const Position& pos) {
+    Color us = pos.side_to_move();
+    return LayerStackBuckets[pos.count<ROOK>(us)][pos.count<ROOK>(~us)]
+                            [pos.count<KNIGHT>(us) + pos.count<CANNON>(us)]
+                            [pos.count<KNIGHT>(~us) + pos.count<CANNON>(~us)];
+}
+
 // Index of a feature for a given king position and another piece on some square
 template<Color Perspective>
-inline IndexType HalfKAv2_hm::make_index(Square s, Piece pc, Square ksq, int att) {
-    return IndexType(IndexMap[KingBuckets[ksq] >> 3][Perspective == BLACK]
-                             [type_of(pc) == ADVISOR || type_of(pc) == BISHOP][s]
-                     + PieceSquareIndex[Perspective][pc]
-                     + PS_NB * ((KingBuckets[ksq] & 0x7) * 9 + att));
+inline IndexType HalfKAv2_hm::make_index(Square s, Piece pc, int bucket, bool mirror) {
+    return IndexType(
+      IndexMap[mirror][Perspective == BLACK][type_of(pc) == ADVISOR || type_of(pc) == BISHOP][s]
+      + PieceSquareIndex[Perspective][pc] + PS_NB * bucket);
 }
 
 // Explicit template instantiations
-template IndexType HalfKAv2_hm::make_index<WHITE>(Square s, Piece pc, Square ksq, int att);
-template IndexType HalfKAv2_hm::make_index<BLACK>(Square s, Piece pc, Square ksq, int att);
+template IndexType HalfKAv2_hm::make_index<WHITE>(Square s, Piece pc, int bucket, bool mirror);
+template IndexType HalfKAv2_hm::make_index<BLACK>(Square s, Piece pc, int bucket, bool mirror);
 
 // Get a list of indices for recently changed features
 template<Color Perspective>
 void HalfKAv2_hm::append_changed_indices(
-  Square ksq, int att, const DirtyPiece& dp, IndexList& removed, IndexList& added) {
+  int bucket, bool mirror, const DirtyPiece& dp, IndexList& removed, IndexList& added) {
     for (int i = 0; i < dp.dirty_num; ++i)
     {
         if (dp.from[i] != SQ_NONE)
-            removed.push_back(make_index<Perspective>(dp.from[i], dp.piece[i], ksq, att));
+            removed.push_back(make_index<Perspective>(dp.from[i], dp.piece[i], bucket, mirror));
         if (dp.to[i] != SQ_NONE)
-            added.push_back(make_index<Perspective>(dp.to[i], dp.piece[i], ksq, att));
+            added.push_back(make_index<Perspective>(dp.to[i], dp.piece[i], bucket, mirror));
     }
 }
 
 // Explicit template instantiations
 template void HalfKAv2_hm::append_changed_indices<WHITE>(
-  Square ksq, int att, const DirtyPiece& dp, IndexList& removed, IndexList& added);
+  int bucket, bool mirror, const DirtyPiece& dp, IndexList& removed, IndexList& added);
 template void HalfKAv2_hm::append_changed_indices<BLACK>(
-  Square ksq, int att, const DirtyPiece& dp, IndexList& removed, IndexList& added);
+  int bucket, bool mirror, const DirtyPiece& dp, IndexList& removed, IndexList& added);
 
 int HalfKAv2_hm::update_cost(const StateInfo* st) { return st->dirtyPiece.dirty_num; }
 
--- src/nnue/nnue_feature_transformer.h
+++ src/nnue/nnue_feature_transformer.h
@@ -50,7 +50,7 @@ static_assert(PSQTBuckets % 8 == 0,
 
 #ifdef USE_AVX512
 using vec_t      = __m512i;
-using psqt_vec_t = __m256i;
+using psqt_vec_t = __m512i;
     #define vec_load(a) _mm512_load_si512(a)
     #define vec_store(a, b) _mm512_store_si512(a, b)
     #define vec_add_16(a, b) _mm512_add_epi16(a, b)
@@ -63,17 +63,17 @@ using psqt_vec_t = __m256i;
     #define vec_slli_16(a, b) _mm512_slli_epi16(a, b)
     // Inverse permuted at load time
     #define vec_packus_16(a, b) _mm512_packus_epi16(a, b)
-    #define vec_load_psqt(a) _mm256_load_si256(a)
-    #define vec_store_psqt(a, b) _mm256_store_si256(a, b)
-    #define vec_add_psqt_32(a, b) _mm256_add_epi32(a, b)
-    #define vec_sub_psqt_32(a, b) _mm256_sub_epi32(a, b)
-    #define vec_zero_psqt() _mm256_setzero_si256()
+    #define vec_load_psqt(a) _mm512_load_si512(a)
+    #define vec_store_psqt(a, b) _mm512_store_si512(a, b)
+    #define vec_add_psqt_32(a, b) _mm512_add_epi32(a, b)
+    #define vec_sub_psqt_32(a, b) _mm512_sub_epi32(a, b)
+    #define vec_zero_psqt() _mm512_setzero_epi32()
     #define NumRegistersSIMD 16
     #define MaxChunkSize 64
 
 #elif USE_AVX512F
 using vec_t      = __m512i;
-using psqt_vec_t = __m256i;
+using psqt_vec_t = __m512i;
     #define vec_op(op, a, b) \
         __builtin_shufflevector(op(__builtin_shufflevector(a, a, 0, 1, 2, 3), \
                                    __builtin_shufflevector(b, b, 0, 1, 2, 3)), \
@@ -95,11 +95,11 @@ using psqt_vec_t = __m256i;
                                 0, 1, 2, 3, 4, 5, 6, 7)
     // Inverse permuted at load time
     #define vec_packus_16(a, b) vec_op(_mm256_packus_epi16, a, b)
-    #define vec_load_psqt(a) _mm256_load_si256(a)
-    #define vec_store_psqt(a, b) _mm256_store_si256(a, b)
-    #define vec_add_psqt_32(a, b) _mm256_add_epi32(a, b)
-    #define vec_sub_psqt_32(a, b) _mm256_sub_epi32(a, b)
-    #define vec_zero_psqt() _mm256_setzero_si256()
+    #define vec_load_psqt(a) _mm512_load_si512(a)
+    #define vec_store_psqt(a, b) _mm512_store_si512(a, b)
+    #define vec_add_psqt_32(a, b) vec_op(_mm256_add_epi32, a, b)
+    #define vec_sub_psqt_32(a, b) vec_op(_mm256_sub_epi32, a, b)
+    #define vec_zero_psqt() _mm512_setzero_si512()
     #define NumRegistersSIMD 16
     #define MaxChunkSize 64
 
@@ -526,9 +526,11 @@ class FeatureTransformer {
 
         // Update incrementally going back through states_to_update.
         // Gather all features to be updated.
-        const Square ksq = pos.king_square(Perspective);
-        const int    att = pos.count<ROOK>(Perspective) * 2 + pos.count<KNIGHT>(Perspective)
-                      + pos.count<CANNON>(Perspective);
+        const Square ksq           = pos.king_square(Perspective);
+        const Square oksq          = pos.king_square(~Perspective);
+        auto [king_bucket, mirror] = FeatureSet::KingBuckets[ksq][oksq];
+        auto attack_bucket         = FeatureSet::make_attack_bucket(pos, Perspective);
+        auto bucket                = king_bucket * 6 + attack_bucket;
 
         // The size must be enough to contain the largest possible update.
         // That might depend on the feature set and generally relies on the
@@ -543,7 +545,7 @@ class FeatureTransformer {
             const StateInfo* end_state = i == 0 ? computed_st : states_to_update[i - 1];
 
             for (StateInfo* st2 = states_to_update[i]; st2 != end_state; st2 = st2->previous)
-                FeatureSet::append_changed_indices<Perspective>(ksq, att, st2->dirtyPiece,
+                FeatureSet::append_changed_indices<Perspective>(bucket, mirror, st2->dirtyPiece,
                                                                 removed[i], added[i]);
         }
 
@@ -734,11 +736,17 @@ class FeatureTransformer {
     void update_accumulator_refresh(const Position& pos, AccumulatorCaches::Cache* cache) const {
         assert(cache != nullptr);
 
-        const Square ksq = pos.king_square(Perspective);
-        const int    att = pos.count<ROOK>(Perspective) * 2 + pos.count<KNIGHT>(Perspective)
-                      + pos.count<CANNON>(Perspective);
+        const Square ksq           = pos.king_square(Perspective);
+        const Square oksq          = pos.king_square(~Perspective);
+        auto [king_bucket, mirror] = FeatureSet::KingBuckets[ksq][oksq];
+        auto attack_bucket         = FeatureSet::make_attack_bucket(pos, Perspective);
+        auto bucket                = king_bucket * 6 + attack_bucket;
 
-        auto& entry = (*cache)[FeatureSet::KingCacheMaps[ksq] * 9 + att][Perspective];
+        auto cache_index = AccumulatorCaches::KingCacheMaps[ksq];
+        if (cache_index < 3 && mirror)
+            cache_index += 9;
+
+        auto& entry = (*cache)[cache_index * 6 + attack_bucket][Perspective];
 
         auto& accumulator                 = pos.state()->accumulator;
         accumulator.computed[Perspective] = true;
@@ -757,12 +765,13 @@ class FeatureTransformer {
                 while (toRemove)
                 {
                     Square sq = pop_lsb(toRemove);
-                    removed.push_back(FeatureSet::make_index<Perspective>(sq, piece, ksq, att));
+                    removed.push_back(
+                      FeatureSet::make_index<Perspective>(sq, piece, bucket, mirror));
                 }
                 while (toAdd)
                 {
                     Square sq = pop_lsb(toAdd);
-                    added.push_back(FeatureSet::make_index<Perspective>(sq, piece, ksq, att));
+                    added.push_back(FeatureSet::make_index<Perspective>(sq, piece, bucket, mirror));
                 }
             }
         }
