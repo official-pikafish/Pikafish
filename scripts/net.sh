d4178d1a4f589726d078f737a3f03b8528b6e4d1
PikaCat <Pika-Cat@qq.com>
2024-09-13 20:01:42 +0800

    Export and clean up net downloading script
    This patch extracts the net downloading script in Makefile into an
    external script file. Also the script is moderately rewritten for
    improved readability and speed.
    
    * Use wget preferentially over curl, as curl is known to have slight
      overhead.
    * Use command instead of hash to check if command exists. Reportedly,
      hash always returns zero in some POSIX shells even when the command
      fails.
    * Command existence checks (wget/curl, sha256sum) are performed only
      once at the beginning.
    * Each of common patterns is encapsulated in a function
      (get_nnue_filename, validate_network).
    * Print out error/warning messages to stderr.
    
    No functional change

---
+++ scripts/net.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+wget_or_curl=$( (command -v wget > /dev/null 2>&1 && echo "wget -q") || \
+                (command -v curl > /dev/null 2>&1 && echo "curl -L -s -k"))
+
+if [ -z "$wget_or_curl" ]; then
+  >&2 printf "%s\n" "Neither wget or curl is installed." \
+	         "Install one of these tools to download NNUE files automatically."
+  exit 1
+fi
+
+fetch_network() {
+  _filename="pikafish.nnue"
+
+  if [ -f "$_filename" ]; then
+    echo "Exists $_filename, skipping download"
+    return
+  fi
+
+  url="https://github.com/official-pikafish/Networks/releases/download/master-net/$_filename"
+    echo "Downloading from $url ..."
+    if $wget_or_curl "$url"; then
+      echo "Successfully downloaded $_filename"
+    else
+      # Download was not successful, return false.
+      echo "Failed to download $_filename from $url"
+      return 1
+    fi
+}
+
+$call fetch_network
