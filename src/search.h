e383966e438c444658854e355dda33b143bea060
Shawn Xu <xu107288696@gmail.com>
2024-11-22 17:13:00 -0800

    Simplify Prior Capture Countermove Bonus

--- src/search.h
+++ src/search.h
@@ -64,7 +64,6 @@ struct Stack {
     CorrectionHistory<PieceTo>* continuationCorrectionHistory;
     int                         ply;
     Move                        currentMove;
-    Piece                       capturedPiece;
     Move                        excludedMove;
     Value                       staticEval;
     int                         statScore;
