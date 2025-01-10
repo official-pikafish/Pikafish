6691ee3125e34bae6933efe778af970e548cebff
Shawn Xu <xu107288696@gmail.com>
2025-01-10 09:11:31 -0800

    Simplify away capthist bonus in Probcut
    The explicit bonus has been obsoleted.

--- src/search.cpp
+++ src/search.cpp
@@ -772,7 +772,6 @@ Value Search::Worker::search(
         assert(probCutBeta < VALUE_INFINITE && probCutBeta > beta);
 
         MovePicker mp(pos, ttData.move, probCutBeta - ss->staticEval, &thisThread->captureHistory);
-        Piece      captured;
 
         while ((move = mp.next_move()) != Move::none())
         {
@@ -785,9 +784,6 @@ Value Search::Worker::search(
                 continue;
 
             assert(pos.capture(move));
-
-            movedPiece = pos.moved_piece(move);
-            captured   = pos.piece_on(move.to_sq());
 
             // Prefetch the TT entry for the resulting position
             prefetch(tt.first_entry(pos.key_after(move)));
@@ -813,8 +809,6 @@ Value Search::Worker::search(
 
             if (value >= probCutBeta)
             {
-                thisThread->captureHistory[movedPiece][move.to_sq()][type_of(captured)] << 1226;
-
                 // Save ProbCut data into transposition table
                 ttWriter.write(posKey, value_to_tt(value, ss->ply), ss->ttPv, BOUND_LOWER,
                                depth - 3, move, unadjustedStaticEval, tt.generation());
