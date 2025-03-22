1158061e0dd09cf7c48e2e717fbe20fbee0523da
Michel Van den Bergh <michel.vandenbergh@uhasselt.be>
2025-03-22 09:58:47 +0100

    Delay check for curl/wget until really needed
    No functional change

--- scripts/net.sh
+++ scripts/net.sh
@@ -3,11 +3,6 @@
 wget_or_curl=$( (command -v wget > /dev/null 2>&1 && echo "wget -qO-") || \
                 (command -v curl > /dev/null 2>&1 && echo "curl -skL"))
 
-if [ -z "$wget_or_curl" ]; then
-  >&2 printf "%s\n" "Neither wget or curl is installed." \
-	         "Install one of these tools to download NNUE files automatically."
-  exit 1
-fi
 
 fetch_network() {
   _filename="pikafish.nnue"
@@ -15,6 +10,12 @@ fetch_network() {
   if [ -f "$_filename" ]; then
     echo "Exists $_filename, skipping download"
     return
+  fi
+
+  if [ -z "$wget_or_curl" ]; then
+    >&2 printf "%s\n" "Neither wget or curl is installed." \
+          "Install one of these tools to download NNUE files automatically."
+    exit 1
   fi
 
   url="https://github.com/official-pikafish/Networks/releases/download/master-net/$_filename"
