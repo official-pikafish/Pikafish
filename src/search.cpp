3929a469d2f910f1f1b948fb56e70f1099e863c8
Daniel Monroe <github.com@danielmonroe.net>
2025-05-26 21:29:43 -0700

    Simplify away check term in statscore

--- src/search.cpp
+++ src/search.cpp
@@ -1120,7 +1120,7 @@ moves_loop:  // When in check, search starts here
         else
             ss->statScore = 2 * thisThread->mainHistory[us][move.from_to()]
                           + (*contHist[0])[movedPiece][move.to_sq()]
-                          + (*contHist[1])[movedPiece][move.to_sq()] + 1000 * ss->inCheck - 4063;
+                          + (*contHist[1])[movedPiece][move.to_sq()] - 4063;
 
         // Decrease/increase reduction for moves with a good/bad history
         r -= ss->statScore * 1223 / 9641;
