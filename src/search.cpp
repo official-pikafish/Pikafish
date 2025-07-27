fde3f9c87f99289ab4b9b0bd103dfed984a12e20
Daniel Monroe <github.com@danielmonroe.net>
2025-07-27 10:06:40 -0400

    Simplify extra continuation history updates

--- src/search.cpp
+++ src/search.cpp
@@ -1736,16 +1736,13 @@ void update_continuation_histories(Stack* ss, Piece pc, Square to, int bonus) {
     const std::array<ConthistBonus, 6> conthist_bonuses = {
       {{1, 1092}, {2, 631}, {3, 294}, {4, 517}, {5, 126}, {6, 445}}};
 
-    static constexpr int conthist_offsets[6]{71, 106, -22, -20, 29, -74};
-
     for (const auto [i, weight] : conthist_bonuses)
     {
         // Only update the first 2 continuation histories if we are in check
         if (ss->inCheck && i > 2)
             break;
         if (((ss - i)->currentMove).is_ok())
-            (*(ss - i)->continuationHistory)[pc][to]
-              << (bonus * weight / 1024) + conthist_offsets[i - 1];
+            (*(ss - i)->continuationHistory)[pc][to] << (bonus * weight / 1024) + 88 * (i < 2);
     }
 }
 
