cb3593f14dd455bfcdcbb037fea411d0816fb798
FauziAkram <fauzi.dabat@hotmail.com>
2025-11-14 01:02:06 +0300

    Simplify NMP reduction formula

--- src/search.cpp
+++ src/search.cpp
@@ -776,7 +776,7 @@ Value Search::Worker::search(
         assert((ss - 1)->currentMove != Move::null());
 
         // Null move dynamic reduction based on depth
-        Depth R = 7 + depth / 3 + improving;
+        Depth R = 8 + depth / 3;
         do_null_move(pos, st, ss);
 
         Value nullValue = -search<NonPV>(pos, ss + 1, -beta, -beta + 1, depth - R, false);
