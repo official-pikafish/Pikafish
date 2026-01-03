ef646466721620e998dc5caf1493006cfe1b4463
Jakub Ciolek <jakub@ciolek.dev>
2026-01-03 23:46:46 +0100

    qsearch: prevent bestValue from going down
    The bestValue can sometimes go down. This happens 2% of the time or so.
    This fix stops it from decreasing.

--- src/search.cpp
+++ src/search.cpp
@@ -1567,7 +1567,7 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta)
                 // we can prune this move.
                 if (!pos.see_ge(move, alpha - futilityBase))
                 {
-                    bestValue = std::min(alpha, futilityBase);
+                    bestValue = std::max(bestValue, std::min(alpha, futilityBase));
                     continue;
                 }
             }
