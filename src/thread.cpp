fed9751722ae63e4828e65598884b31db32a536d
aba2222 <abcsss2035@gmail.com>
2026-07-26 12:51:49 +0200

    avoid duplicate legal move check
    Remove redundant legal move checks when search moves are limited. Because `UCIEngine::to_move` already performs this check.
    
    Also remove the unused `legalMoves` variable.
    
    No functional change

--- src/thread.cpp
+++ src/thread.cpp
@@ -293,18 +293,17 @@ void ThreadPool::start_thinking(Position& pos, StateListPtr& states, Search::Lim
     increaseDepth = true;
 
     Search::RootMoves rootMoves;
-    const auto        legalmoves = MoveList<LEGAL>(pos);
 
     for (const auto& uciMove : limits.searchmoves)
     {
         auto move = UCIEngine::to_move(pos, uciMove);
 
-        if (std::find(legalmoves.begin(), legalmoves.end(), move) != legalmoves.end())
+        if (move != Move::none())
             rootMoves.emplace_back(move);
     }
 
     if (rootMoves.empty())
-        for (const auto& m : legalmoves)
+        for (const auto& m : MoveList<LEGAL>(pos))
             rootMoves.emplace_back(m);
 
     // After ownership transfer 'states' becomes empty, so if we stop the search
